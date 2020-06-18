cfb16b3a55f6401f684e358f302487fc06e5677e
Den4200 <dpham.42@hotmail.com>
2020-06-18 02:09:45 -0400

    implement github api usage for repository data

--- portfolio/models.py
+++ portfolio/models.py
@@ -3,12 +3,12 @@ from django.db import models
 
 class RepositoryMetadata(models.Model):
     name = models.CharField(max_length=256)
-    short_description = models.TextField(max_length=1024)
-    long_description = models.TextField(max_length=4096)
+    short_description = models.TextField(max_length=1024, blank=True, null=True)
+    long_description = models.TextField(max_length=4096, blank=True, null=True)
 
-    language = models.CharField(max_length=64)
+    language = models.CharField(max_length=64, blank=True, null=True)
 
-    stargazers = models.IntegerField()
-    forks = models.IntegerField()
+    stargazers = models.IntegerField(blank=True, null=True)
+    forks = models.IntegerField(blank=True, null=True)
 
     last_updated = models.DateTimeField(auto_now=True)
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,9 +1,53 @@
+from datetime import datetime, timezone
+
 from django.shortcuts import render
 from django.views import View
+import requests
+
+from portfolio.models import RepositoryMetadata
 
 
 class IndexView(View):
     template_name = 'portfolio/index.html'
 
+    github_api = "https://api.github.com/users/Den4200/repos"
+    repository_cache_ttl = 3600
+
     def get(self, request):
         return render(request, self.template_name)
+
+    def _update_repo_data(self):
+        repos = RepositoryMetadata.objects.all()
+        api_data = requests.get(self.github_api).json()
+
+        for repo_data in api_data:
+            full_name = repo_data['full_name']
+            print(full_name)
+
+            try:
+                repo = repos.get(name=full_name)
+
+            except RepositoryMetadata.DoesNotExist:
+                continue
+
+            else:
+                repo.short_description = repo_data['description']
+                repo.language = repo_data['language']
+                repo.forks = repo_data['forks_count']
+                repo.stargazers = repo_data['stargazers_count']
+
+                repo.save()
+
+    def _get_repo_data(self):
+        repos = RepositoryMetadata.objects.all()
+
+        if not repos:
+            return repos
+
+        if any(
+            (datetime.now(tz=timezone.utc) - repo.last_updated).seconds >= self.repository_cache_ttl
+            for repo in repos
+        ):
+            self._update_repo_data()
+
+        return repos
