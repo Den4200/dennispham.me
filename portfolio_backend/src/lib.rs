b6ac27ce9c33883080bb6ecda60e63635f854016
Den4200 <dennis@dennispham.me>
2020-12-28 14:05:24 -0500

    Add automatic database migrations.

--- portfolio_backend/src/lib.rs
+++ portfolio_backend/src/lib.rs
@@ -2,6 +2,8 @@
 
 #[macro_use]
 extern crate diesel;
+#[macro_use]
+extern crate diesel_migrations;
 
 #[macro_use]
 extern crate rocket;
@@ -16,10 +18,28 @@ use rocket::Rocket;
 mod config;
 mod db;
 
+embed_migrations!();
+
+fn run_migrations(rocket: Rocket) -> Result<Rocket, Rocket> {
+    let conn = db::Conn::get_one(&rocket).expect("Failed to get database connection..");
+
+    match embedded_migrations::run_with_output(&*conn, &mut std::io::stdout()) {
+        Ok(()) => {
+            println!("Successfully ran database migrations.");
+            Ok(rocket)
+        }
+        Err(e) => {
+            println!("Failed to run database migrations: {:?}", e);
+            Err(rocket)
+        }
+    }
+}
+
 pub fn rocket() -> Rocket {
     dotenv().ok();
 
     rocket::custom(config::from_env())
         .mount("/", routes![])
         .attach(db::Conn::fairing())
+        .attach(AdHoc::on_attach("Database Migrations", run_migrations))
 }
