a18907a5df7c257f0500112dc0ae34cfab5fad43
Den4200 <dennis@dennispham.me>
2020-12-31 00:07:29 -0500

    Add Repository interface.

--- portfolio_frontend/src/api.ts
+++ portfolio_frontend/src/api.ts
@@ -4,6 +4,26 @@ const ApiClient = axios.create({
     baseURL: "https://dennispham.me/api/"
 })
 
-export async function getRepositories() {
-    return (await ApiClient.get("repositories")).data;
+export interface Repository {
+    name: string,
+    description: string,
+    language: string,
+    stargazers: number,
+    forks: number,
+    ordering: number,
+    lastUpdated: string
+}
+
+export async function getRepositories(): Promise<Repository[]> {
+    let data = (await ApiClient.get("repositories")).data;
+
+    return data.map((repository: any) => {
+        let lastUpdated = repository.last_updated;
+        delete repository.last_updated;
+
+        return {
+            lastUpdated,
+            ...repository
+        } as Repository;
+    });
 }
