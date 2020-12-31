aca8afa8b41f395b4f0e3c9467822c8faf80c2ee
Den4200 <dennis@dennispham.me>
2020-12-30 22:25:27 -0500

    Add navbar component.

--- portfolio_frontend/src/App.tsx
+++ portfolio_frontend/src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
   return (
     <div className="App">
---
+++ portfolio_frontend/src/components/navbar/NavigationBar.tsx
@@ -0,0 +1,27 @@
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Link } from 'react-router-dom';
+
+import './navbar.css';
+
+function NavigationBar() {
+  return (
+    <Navbar collapseOnSelect bg="dark-opaque" variant="dark" fixed="top" expand="md">
+      <Navbar.Brand><Link className="navbar-brand-link" to="/">Dennis Pham</Link></Navbar.Brand>
+
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="navbar-nav ml-auto">
+          <LinkContainer className="pr-3" to="/">
+            <Nav.Link>Home</Nav.Link>
+          </LinkContainer>
+          <Nav.Link className="nav-link pr-3" href="#">Projects</Nav.Link>
+          <Nav.Link className="nav-link pr-3" href="#">About</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  )
+}
+
+export default NavigationBar;
