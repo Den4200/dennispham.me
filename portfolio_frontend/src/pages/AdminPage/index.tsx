fc1c6bd6f80508ee951896db78e9c7f0d0c34e86
Den4200 <dennis@dennispham.me>
2021-01-25 14:20:18 -0500

    Add missing semicolon.

--- portfolio_frontend/src/pages/AdminPage/index.tsx
+++ portfolio_frontend/src/pages/AdminPage/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCodeBranch, faStar } from '@fortawesome/free-solid-svg-icons';
 
 import { Repository, addRepository, getRepositories, removeRepository } from '../../api';
