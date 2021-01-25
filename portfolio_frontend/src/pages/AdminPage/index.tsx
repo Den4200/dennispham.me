887a92125b731aa447d2a3fba25c6b0594d8aa75
Den4200 <dennis@dennispham.me>
2021-01-25 12:29:42 -0500

    Display repositories on the admin page.
    Along with checkboxes (which do nothing for now)!

--- portfolio_frontend/src/pages/AdminPage/index.tsx
+++ portfolio_frontend/src/pages/AdminPage/index.tsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCodeBranch, faStar } from '@fortawesome/free-solid-svg-icons';
 
 import { Repository, addRepository, getRepositories, removeRepository } from '../../api';
 import Loading from '../../components/Loading';
@@ -21,9 +23,31 @@ const AdminPage = () => {
     );
   }
 
+  const toGitHubLink = (repoName: string) => `https://github.com/${repoName}`;
+
   return (
     <div className="AdminPage">
       <Sidebar />
+
+      <div className="AdminPage-content">
+        <h1>Repositories</h1>
+
+        <table className="AdminPage-table">
+          {repositories.map(repo => (
+            <tr>
+              <td>
+                <label className="AdminPage-table-checkbox">
+                  <input type="checkbox" name={repo.name} />
+                  <span className="AdminPage-table-checkmark" />
+                </label>
+              </td>
+              <td><a href={toGitHubLink(repo.name)}>{repo.name}</a></td>
+              <td><FontAwesomeIcon icon={faStar} /> {repo.stargazers}</td>
+              <td><FontAwesomeIcon icon={faCodeBranch} /> {repo.forks}</td>
+            </tr>
+          ))}
+        </table>
+      </div>
     </div>
   );
 };
