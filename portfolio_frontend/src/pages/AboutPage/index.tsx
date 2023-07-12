0d719faca964cd51db78cd93e072dd107cc97f68
Dennis Pham <dennis@dennispham.me>
2023-07-12 13:13:06 -0400

    Format about page code.

--- portfolio_frontend/src/pages/AboutPage/index.tsx
+++ portfolio_frontend/src/pages/AboutPage/index.tsx
@@ -10,7 +10,10 @@ const AboutPage = () => (
         <h1>About Me</h1>
         <div className="heading-underline"></div>
       </div>
-      <div className="about-inner" style={{ backgroundImage: `url(${aboutHeaderImage})` }}></div>
+      <div
+        className="about-inner"
+        style={{ backgroundImage: `url(${aboutHeaderImage})` }}
+      ></div>
       &nbsp;
     </div>
 
@@ -18,9 +21,10 @@ const AboutPage = () => (
       <div className="content">
         <h3 className="heading mb-3">Hey there! My name is Dennis.</h3>
         <p className="text">
-          I am an aspiring software engineer studying Computer Science, living in the United States. I enjoy
-          playing guitar, spending time with friends and family, and last but not least,
-          programming. Learn more about me below!
+          I am an aspiring software engineer studying Computer Science at Purdue
+          Student, living in the United States. I enjoy playing guitar, spending
+          time with friends and family, and last but not least, programming.
+          Learn more about me below!
         </p>
 
         <hr className="mt-4" />
@@ -60,9 +64,10 @@ const AboutPage = () => (
         <h3 className="heading mb-3">Projects</h3>
         <ul>
           <li className="text">
-            Administrator of <a href="https://pythondiscord.com/">Python Discord</a>, a community
-            focused around the Python language, striving to create new developers with over 300,000
-            members.
+            Administrator of{" "}
+            <a href="https://pythondiscord.com/">Python Discord</a>, a community
+            focused around the Python language, striving to create new
+            developers with over 300,000 members.
           </li>
           <li className="text">
             Participated in the Python Discord{" "}
@@ -70,11 +75,15 @@ const AboutPage = () => (
               Winter Code Jam 2020
             </a>
             &nbsp;and{" "}
-            <a href="https://pythondiscord.com/pages/events/game-jam-2020/">Game Jam 2020</a>,
-            before becoming an administrator. My teams placed in the top 3 in both jams.
+            <a href="https://pythondiscord.com/pages/events/game-jam-2020/">
+              Game Jam 2020
+            </a>
+            , before becoming an administrator. My teams placed in the top 3 in
+            both jams.
           </li>
           <li className="text">
-            Various projects updated hourly on my <Link to="/projects">projects page</Link>.
+            Various projects updated hourly on my{" "}
+            <Link to="/projects">projects page</Link>.
           </li>
         </ul>
 
@@ -83,14 +92,16 @@ const AboutPage = () => (
         <h3 className="heading mb-3">Socials</h3>
         <ul>
           <li className="text">
-            Email: <a href="mailto:dennis@dennispham.me">dennis@dennispham.me</a>
+            Email:{" "}
+            <a href="mailto:dennis@dennispham.me">dennis@dennispham.me</a>
           </li>
           <li className="text">Discord: dennispham.me</li>
           <li className="text">
             GitHub: <a href="https://www.github.com/Den4200">Den4200</a>
           </li>
           <li className="text">
-            LinkedIn: <a href="https://www.linkedin.com/in/dennispham0/">Dennis Pham</a>
+            LinkedIn:{" "}
+            <a href="https://www.linkedin.com/in/dennispham0/">Dennis Pham</a>
           </li>
         </ul>
       </div>
